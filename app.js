d82ed71398e1055e04d481379523a74652095b06
Abhirup Sarkar <76419771+Abhirup-Sarkar@users.noreply.github.com>
2021-06-15 23:09:36 +0530

    fixing input output

--- app.js
+++ app.js
@@ -1,19 +1,55 @@
-//  var userName = prompt("What is your name? ");
-// alert("Welcome! "+userName);
 
-//Input
+
+//click button
 var btnTranslate = document.querySelector("#btn-translate");
+
+
+//read input
 var txtInput = document.querySelector("#txt-input");
-var outputDiv = document.querySelector("#output"); 
-console.log(txtInput)
-// outputDiv.innerText = "Abhirup Sarkar"
+//show output
+var outputDiv = document.querySelector("#output");
 
-function clickHandler(){
+//server URL
 
-    console.log("clicked!");
-    console.log("input", txtInput.value)
-   outputDiv.innerText = "Minion Translator:  "+txtInput.value;
-};
+var serverURL = "https://api.funtranslations.com/translate/yoda.json";
 
-btnTranslate.addEventListener("click",clickHandler)
- 
+function getTranslationURL(text) {
+    return serverURL + "?" + "text = "+text;
+}
+
+// Error Handling
+
+function errorHandler(error) {
+    console.log("error occured", error);
+    alert("something wrong with the server! Try again after some time.");
+}
+
+
+// function clickHandler() {
+//     console.log("Clicked! ");
+//     var inputText = txtInput.value; //Taking input
+       
+//     //calling server for processing 
+
+//     fetch(getTranslationURL(inputText))
+//         .then(response => response.json())
+//         .then(json =>console.log(json.contents.translated))
+//         .catch(errorHandler)
+//         outputDiv.innerText = translatedText;  
+        
+        
+// };
+function clickHandler(event) {
+    console.log("button clicked");
+    var inputText = txtInput.value;
+    
+    fetch(getTranslationURL(inputText))
+        .then(response => response.json())
+        .then(json => {
+            outputDiv.innerText = json.contents.translated;
+        })
+        .catch(() => alert("some error occured"))
+    
+}
+btnTranslate.addEventListener("click", clickHandler);
+
