3424e65552bc27b6955624d64eac26dd10829614
Abhirup Sarkar <76419771+Abhirup-Sarkar@users.noreply.github.com>
2021-06-15 17:32:02 +0530

    fixed syntax

--- app.js
+++ app.js
@@ -4,13 +4,14 @@
 //Input
 var btnTranslate = document.querySelector("#btn-translate");
 var txtInput = document.querySelector("#txt-input");
-console.log(txtInput);
+console.log(txtInput)
 
 function clickHandler(){
 
-    console.log("Clicked!");
+    console.log("clicked!");
     console.log("input", txtInput.value);
    
 };
+
 btnTranslate.addEventListener("click",clickHandler)
  
