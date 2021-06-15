ca470f3ac45decf900521490162a7ba4fd054d83
Abhirup Sarkar <76419771+Abhirup-Sarkar@users.noreply.github.com>
2021-06-15 09:10:59 +0530

    added click button

---
+++ app.js
@@ -0,0 +1,13 @@
+//  var userName = prompt("What is your name? ");
+// alert("Welcome! "+userName);
+
+//Input
+var btnTranslate = document.querySelector("#btn-translate");
+
+btnTranslate.addEventListener("click",clickHandler())
+ 
+
+function clickHandler(){
+
+    console.log("Clicked!");
+}
