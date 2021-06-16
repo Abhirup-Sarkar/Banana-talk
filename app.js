e26b2c1f9b2fb7d2171e7fd541746885d6fc233b
Abhirup Sarkar <76419771+Abhirup-Sarkar@users.noreply.github.com>
2021-06-16 22:54:25 +0530

    fixed input output

--- app.js
+++ app.js
@@ -3,26 +3,44 @@
 //click button
 var btnTranslate = document.querySelector("#btn-translate");
 
+btnTranslate.addEventListener("click", clickHandler);
 
 //read input
 var txtInput = document.querySelector("#txt-input");
 //show output
-var outputDiv = document.querySelector("#output");
+var txtOutput = document.querySelector("#txt-output");
 
 //server URL
 
-var serverURL = "https://api.funtranslations.com/translate/yoda.json";
+var serverURL = "https://api.funtranslations.com/translate/minion.json";
 
-function getTranslationURL(text) {
-    return serverURL + "?" + "text = "+text;
+
+
+
+function clickHandler(event) {
+    console.log("button clicked");
+    var input = txtInput.value;
+    var finalURL = getTranslationURL(input);
+    console.log(finalURL);
+    fetch(finalURL)
+        .then(response => response.json())
+        .then(json =>{
+            txtOutput.innerText = json.contents.translated;
+            
+        })
+        .catch(() => alert("some error occured"));
+    
+}
+function getTranslationURL(inputText) {
+    return serverURL + "?" + "text = "+inputText;
 }
 
 // Error Handling
 
-function errorHandler(error) {
-    console.log("error occured", error);
-    alert("something wrong with the server! Try again after some time.");
-}
+// function errorHandler(error) {
+//     console.log("error occured", error);
+//     alert("something wrong with the server! Try again after some time.");
+// }
 
 
 // function clickHandler() {
@@ -39,17 +57,3 @@ function errorHandler(error) {
         
         
 // };
-function clickHandler(event) {
-    console.log("button clicked");
-    var inputText = txtInput.value;
-    
-    fetch(getTranslationURL(inputText))
-        .then(response => response.json())
-        .then(json => {
-            outputDiv.innerText = json.contents.translated;
-        })
-        .catch(() => alert("some error occured"))
-    
-}
-btnTranslate.addEventListener("click", clickHandler);
-
